f2a338b20bd774798e67c95dd88b16c2b595c162
zaquastier <davidvaldiviadonoyan@gmail.com>
2024-04-04 22:49:14 +0200

    Split notebooks

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import ot
+from scipy.sparse import csr_matrix
 
 def dirac_distribution(frequencies, values, n=1000):
     if not len(frequencies) == len(values):
@@ -36,4 +38,28 @@ def get_frequency(f_i, f_j, alpha, method='int'):
         return int((1 - alpha) * f_i + alpha * f_j)
     if method == 'round':
         return round((1 - alpha) * f_i + alpha * f_j)
+    
+def emd_optimal_transport(frequency, a1, a2, alpha=0.5): # can i get rid of frequency?
+
+    emd_plan = ot.emd_1d(frequency, frequency, a1, a2)
+
+    emd_plan = csr_matrix(emd_plan)
+    emd_interpolation = np.zeros(len(frequency))
+    row, col = emd_plan.nonzero()
+    for i, j in zip(row, col):
+        index = get_frequency(i, j, alpha=alpha, method='int')
+        emd_interpolation[index] += emd_plan[i, j]
+    
+    return emd_interpolation
+
+
+# STFT    
+
+def normalized_frame(stft, index):
+    frame = stft[:, index]
+    frame = np.abs(frame)
+    frame_relative_amplitude = np.sum(frame)
+    frame = frame / frame_relative_amplitude
+
+    return frame, frame_relative_amplitude
     
